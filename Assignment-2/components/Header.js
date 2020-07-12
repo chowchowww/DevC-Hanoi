ef7f6ea341a0787bd565c40a450053b5f2081d65
Chow <chaulea@gmail.com>
2020-07-12 22:31:17 +0700

    css

---
+++ Assignment-2/components/Header.js
@@ -0,0 +1,119 @@
+import React from 'react';
+import { View, StyleSheet, Text, SafeAreaView , Image, Button } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+
+const POLO_BLUE_COLOR = 'rgb(51,60,87)';
+const FOLLOW_COLOR = 'rgb(71,113,246)';
+const SEND_MESSAGE_COLOR = 'rgb(120,213,250)';
+
+export default function Header() {
+    return (
+        <View>
+            <View style={styles.header}>
+                <MaterialIcons name="arrow-back" size={24} color={POLO_BLUE_COLOR} />
+                <MaterialIcons name="more-horiz" size={24} color={POLO_BLUE_COLOR} />
+            </View>
+            <View style={styles.information}>
+                <Image
+                    style={styles.avatar}
+                    source={require('../assets/avt3.png')}
+                    resizeMode='cover'
+                />
+                <View>
+                    <Text style={styles.titleName}>Unsplash Photo</Text>
+                    <Text style={styles.jobName}>Photographer</Text>
+                    <View style={styles.rowButton}>
+                        <View style={styles.followButton}>
+                            <Button
+                                title='Follow'
+                                color='white'
+                            />
+                        </View>
+                        <View style={styles.sendMessageButton}>
+                            <MaterialIcons name="send" size={24} color="white" />
+                        </View>
+                    </View>
+                </View>
+            </View>
+            <View style={styles.followContent}>
+                <View style={styles.textBox}>
+                    <Text style={styles.bigText}>210</Text>
+                    <Text style={styles.smallText}>Photos</Text>
+                </View>
+                <View style={styles.textBox}>
+                    <Text style={styles.bigText}>15k</Text>
+                    <Text style={styles.smallText}>Followers</Text>
+                </View>
+                <View style={styles.textBox}>
+                    <Text style={styles.bigText}>605</Text>
+                    <Text style={styles.smallText}>Following</Text>
+                </View>
+            </View>
+        </View>
+    );
+}
+const styles = StyleSheet.create({
+    header: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginHorizontal: 10,
+        marginVertical: 10,
+    },
+    information: {
+        flexDirection: 'row',
+        marginHorizontal: 10,
+        marginVertical: 10,
+        justifyContent: 'center'
+    },
+    avatar: {
+        borderRadius: 23,
+        width: 90,
+        height: 90,
+        marginRight: 30,
+    },
+    titleName: {
+        fontSize: 24,
+        fontWeight: 'bold',
+    },
+    jobName: {
+        fontSize: 16,
+        opacity: 0.3
+    },
+    rowButton: {
+        flexDirection: 'row'
+    },
+    followButton: {
+        width: 120,
+        borderRadius: 25,
+        backgroundColor: FOLLOW_COLOR,
+        marginHorizontal: 2,
+        marginVertical: 10,
+    },
+    sendMessageButton: {
+        width: 40,
+        borderRadius: 30,
+        backgroundColor: SEND_MESSAGE_COLOR,
+        marginVertical: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    followContent: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        marginHorizontal: 20,
+        marginVertical: 30,
+    },
+    textBox: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    bigText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    smallText: {
+        fontSize: 14,
+        opacity: 0.3,
+    },
+});
---
+++ Assignment-2/components/ListImage.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+
+const imgData = [
+    {id :1, src: require('../assets/pic1.jpg')},
+    {id :2, src: require('../assets/pic2.jpg')},
+    {id :3, src: require('../assets/pic3.jpg')},
+    {id :4, src: require('../assets/pic4.jpg')},
+    {id :5, src: require('../assets/pic5.jpg')},
+    {id :6, src: require('../assets/pic6.jpg')}
+];
+
+const centerImgData = Math.floor(imgData.length / 2);
+
+export default function ListImage() {
+    return (
+        <ScrollView
+            contentContainerStyle={{
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}
+        >
+            <View
+                style={{
+                    flexDirection: 'column'
+                }}
+            >
+                {imgData.slice(0, centerImgData).map(item => <Image source={item.src} style={styles.image} />)}
+            </View>
+            <View
+                style={{
+                    flexDirection: 'column'
+                }}
+            >
+                {imgData.slice(centerImgData).map(item => <Image source={item.src} style={styles.image} />)}
+            </View>
+        </ScrollView>
+    );
+}
+
+const styles = StyleSheet.create({
+    image: {
+        marginHorizontal: 5,
+        marginVertical: 5,
+        borderRadius: 25,
+    }
+});
