68b57a7a14545a6bfa028e4c1acdf0469fdb3307
Chow <chaulea@gmail.com>
2020-07-15 19:14:22 +0700

    milestones 4

--- Lab-3/App.js
+++ Lab-3/App.js
@@ -1,18 +1,62 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { StyleSheet, Text, View, TextInput, SafeAreaView, TouchableOpacity } from 'react-native';
 
-const ConversionTypeButton = props => {
-  const fromFlag = props.from === 'usd' ? '🇺🇸' : '🇻🇳';
-  const toFlag = props.to === 'usd' ? '🇺🇸' : '🇻🇳';
+const ConversionTypeButton = ({ fromCurrency, toCurrency, from, to, setConversionCurrencies }) => {
+  const backgroundColor =
+    fromCurrency === from && toCurrency === to ? 'lightblue' : null;
+  const buttonStyle = {backgroundColor: backgroundColor}
+  const fromFlag = from === 'usd' ? '🇺🇸' : '🇻🇳';
+  const toFlag = to === 'usd' ? '🇺🇸' : '🇻🇳';
+
   return (
-  <TouchableOpacity style={styles.button}>
+  <TouchableOpacity
+    style={[styles.button, buttonStyle]}
+    onPress={() => setConversionCurrencies(from, to)}
+  >
     <Text>{fromFlag} to {toFlag}</Text>
   </TouchableOpacity>
   );
 }
 
+const FormattedCurrency = ({ type, value }) => {
+  const format = type === 'usd' ? 'us' : 'vn';
+  const currency = type === 'usd' ? 'USD' : 'VND';
+  const flag = type === 'usd' ? '🇺🇸' : '🇻🇳';
+
+  const formatter = new Intl.NumberFormat(format, {
+    currency,
+    style: 'currency'
+  });
+
+  return (
+    <Text style={styles.currencyText}>
+      {formatter.format(value)} {flag}
+    </Text>
+  );
+};
+
 export default function App() {
+  const [currentCurrencyValue, setFromCurrencyValue] = useState(0);
+  const [convertedCurrencyValue, setConvertedValue] = useState(0);
+  const [toCurrency, setToCurrency] = useState('usd');
+  const [fromCurrency, setFromCurrency] = useState('vnd');
+
+  const convertCurrency = () => {
+    if (fromCurrency === 'vnd') {
+      setConvertedValue(currentCurrencyValue / 23000)
+    } else {
+      setConvertedValue(23000*currentCurrencyValue)
+    }
+  }
+
+  const setConversionCurrencies = (from, to) => {
+    setToCurrency(to);
+    setFromCurrency(from);
+  };
+
+  useEffect(convertCurrency)
+
   return (
     <SafeAreaView style={styles.container}>
       <Text>Please enter the value of the currency you want to convert</Text>
@@ -23,13 +67,36 @@ export default function App() {
         placeholder='100,000,000 VND'
         selectionColor='red'
         style={styles.textInput}
+        onChangeText={setFromCurrencyValue}
       />
-      <ConversionTypeButton to='usd' from='vnd'/>
-      <ConversionTypeButton to='vnd' from='usd'/>
-      <Text>Current currency:</Text>
-      <Text style={styles.currencyText}>0.00</Text>
-      <Text>Conversion currenecy:</Text>
-      <Text style={styles.currencyText}>0.00</Text>
+      <ConversionTypeButton
+        toCurrency={toCurrency}
+        fromCurrency={fromCurrency}
+        setConversionCurrencies={setConversionCurrencies}
+        from='vnd'
+        to='usd'
+      />
+      <ConversionTypeButton
+        toCurrency={toCurrency}
+        fromCurrency={fromCurrency}
+        setConversionCurrencies={setConversionCurrencies}
+        from='usd'
+        to='vnd'
+      />
+      <Text>
+        Current currency:
+      </Text>
+      <FormattedCurrency
+        type={fromCurrency}
+        value={currentCurrencyValue}
+      />
+      <Text>
+        Conversion currenecy:
+      </Text>
+      <FormattedCurrency
+        type={toCurrency}
+        value={convertedCurrencyValue}
+      />
     </SafeAreaView>
   );
 }
