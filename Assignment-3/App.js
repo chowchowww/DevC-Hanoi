cbb6678e01ddc5d6d3a9ab863b4ffacac7d8e6ef
Chow <chaulea@gmail.com>
2020-07-19 21:16:22 +0700

    final

--- Assignment-3/App.js
+++ Assignment-3/App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import Button from './components/Button';
 import Card from './components/Card';
 
@@ -23,6 +23,10 @@ export default function App() {
   const [gamePrompt, setGamePrompt] = useState('Choose your wapon!');
   const [userChoice, setUserChoice] = useState({});
   const [computerChoice, setComputerChoice] = useState({});
+  const [gamePlayed, setGamePlayed] = useState(0);
+  const [history, setHistory] = useState([]);
+  const [winGames, setWinGames] = useState(0);
+  const [loseGames, setLoseGames] = useState(0);
 
   const getResultColor = () => gamePrompt === 'Victory!' ? 'green' : gamePrompt === 'Defeat!' ? 'red' : null;
 
@@ -49,30 +53,42 @@ export default function App() {
     const newUserChoice = CHOICES.find(choice => choice.name === playerChoice);
     const newComputerChoice = CHOICES.find(choice => choice.name === compChoice);
 
+    result === 'Victory!' ? setWinGames(winGames+1) : result === 'Defeat!' ? setLoseGames(loseGames+1) : null;
+
     setGamePrompt(result);
     setUserChoice(newUserChoice);
     setComputerChoice(newComputerChoice);
+    setHistory(history.concat(result));
+    setGamePlayed(gamePlayed+1);
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={{ fontSize: 35, color: getResultColor() }}>{gamePrompt}</Text>
-      <View style={styles.choicesContainer}>
-          <Card player='Player' choice={userChoice}/>
-          <Text style={{ color: '#250902'}}>vs</Text>
-          <Card player='Computer' choice={computerChoice}/>
+    <ScrollView>
+      <View style={styles.container}>
+        <Text style={{ fontSize: 35, color: getResultColor() }}>{gamePrompt}</Text>
+        <Text>Game played: {gamePlayed}</Text>
+        <View style={styles.choicesContainer}>
+            <Card player='Player' choice={userChoice}/>
+            <Text style={{ color: '#250902'}}>vs</Text>
+            <Card player='Computer' choice={computerChoice}/>
+        </View>
+        <View style={styles.buttonsContainer}>
+          {
+            CHOICES.map(choice => <Button key={choice.name} name={choice.name} onPress={onPress} />)
+          }
+        </View>
+        <View>
+            <Text>Win rate: Win {Math.floor((winGames/history.length)*100)}%, Lose {Math.floor((loseGames/history.length)*100)}%, Tie {Math.floor((history.length-winGames-loseGames)/history.length*100)}%</Text>
+            <Text>History: { history.map(h => h + ', ') }</Text>
+        </View>
       </View>
-      <View style={styles.buttonsContainer}>
-        {
-          CHOICES.map(choice => <Button key={choice.name} name={choice.name} onPress={onPress} />)
-        }
-      </View>
-    </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 15,
     flex: 1,
     backgroundColor: '#e9ebee',
     alignItems: 'center',
